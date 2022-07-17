de2a00316ac6fa1b09ba2de291108e0970213957
Anthony Thorpe <59274215+contrarythought@users.noreply.github.com>
2022-07-16 23:07:25 -0400

    Update log.go

--- distr/proglog/internal/server/log.go
+++ distr/proglog/internal/server/log.go
@@ -5,33 +5,40 @@ import (
 	"sync"
 )
 
-type Log struct {
-	mu      sync.Mutex
-	records []Record
-}
-
-func NewLog() *Log {
-	return new(Log)
-}
-func (c *Log) Append(record Record) (uint64, error) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	record.Offset = uint64(len(c.records))
-	c.records = append(c.records, record)
-	return record.Offset, nil
-}
-func (c *Log) Read(offset uint64) (Record, error) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	if offset >= uint64(len(c.records)) {
-		return Record{}, ErrOffsetNotFound
-	}
-	return c.records[offset], nil
-}
-
-var ErrOffsetNotFound = fmt.Errorf("offset not found")
-
 type Record struct {
 	Value  []byte `json:"value"`
 	Offset uint64 `json:"offset"`
+}
+
+type Log struct {
+	records []Record
+	mu      sync.Mutex
+}
+
+func NewLog() *Log {
+	return &Log{}
+}
+
+// append a record to the log
+func (l *Log) Append(record Record) uint64 {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	record.Offset = uint64(len(l.records))
+	l.records = append(l.records, record)
+	return record.Offset
+}
+
+var ErrOutOfBounds = fmt.Errorf("offset out of bounds")
+
+// retrieve a record from the log
+func (l *Log) Read(offset uint64) (Record, error) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	if offset >= uint64(len(l.records)) {
+		return Record{}, ErrOutOfBounds
+	}
+
+	return l.records[offset], nil
 }
